9b9aa9e96b3bfeb2ee7f164a5885527c7d748369
Hasti Ahoei <97366164+Hastiahoei@users.noreply.github.com>
2022-07-21 16:28:07 +0200

    Update foreach.js

--- foreach.js
+++ foreach.js
@@ -13,3 +13,25 @@ myArray.forEach(myFunction);
 
  console.log(myArray);
 
+
+//array
+let maennlich = ['Ralf', 'Erwin', 'Jürgen', 'Harald', 'Stefan'];
+let weiblich = ['Erika', 'Julia', 'Rosamunde', 'Melanie', 'Jasmin'];
+let nachnamen = ['Schmidt', 'Müller', 'Meyer', 'Schulz', 'Kowalski'];
+
+const zufallErzeugen = (min, max) => ~~(Math.random() * (max - min + 1) + min);
+
+const zufallsAuswahl = (sammlung, anzahl) => {
+    let auswahl = [];
+    sammlung = [...sammlung];
+    for (let i = 0; i < anzahl; i++) {
+        auswahl.push(sammlung.splice(zufallErzeugen(0, sammlung.length - 1), 1)[0]);
+    }
+    return auswahl;
+}
+
+let name = zufallsAuswahl(weiblich, zufallErzeugen(1,2)).join('-');
+name += ' ';
+name += zufallsAuswahl(nachnamen, zufallErzeugen(1,2)).join('-');
+
+console.log(name);
